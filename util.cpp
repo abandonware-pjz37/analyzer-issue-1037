7359e58dfbf4daba8b796b8b308b97e463f876f3
Isaac Hier <ihier@uber.com>
2017-09-14 14:31:50 -0400

    Add assertions.

--- util.cpp
+++ util.cpp
@@ -1,11 +1,14 @@
 #include "util.h"
 
+#include <cassert>
+
 uint64_t shift(int num_bits)
 {
     int x = 0;
     for (int i = 0; i < num_bits; ++i) {
         x += 1;
     }
+    assert(x > 0);
     return static_cast<uint64_t>(1) << (64 - x);
 }
 
@@ -15,5 +18,6 @@ int divide(int length)
     for (int i = 0; i < length; ++i) {
         x += 1;
     }
+    assert(x > 0);
     return length / x;
 }
