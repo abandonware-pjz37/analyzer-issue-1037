2d0ccfe3f482b34e1254ebf34327fc4c369697fb
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-09-15 21:11:06 +0300

    Use 'my_assert'

--- foo.cpp
+++ foo.cpp
@@ -1,5 +1,29 @@
 #include <cassert>
 #include <cstdint>
+#include <cstdlib> // abort
+
+#if defined(__clang_analyzer__)
+# define my_assert(A) \
+  do { \
+    if (!(A)) { \
+      __builtin_unreachable(); \
+    } \
+  } \
+  while (false)
+#elif defined(NDEBUG)
+# define my_assert(A) \
+  do { \
+  } \
+  while (false)
+#else
+# define my_assert(A) \
+  do { \
+    if (!(A)) { \
+      abort(); \
+    } \
+  } \
+  while (false)
+#endif
 
 uint64_t foo(int num_bits)
 {
@@ -7,6 +31,6 @@ uint64_t foo(int num_bits)
     for (int i = 0; i < num_bits; ++i) {
         x += 1;
     }
-    assert(x > 0);
+    my_assert(x > 0);
     return static_cast<uint64_t>(1) << (64 - x);
 }
